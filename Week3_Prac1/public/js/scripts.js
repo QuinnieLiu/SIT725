f06fdfc31d116258a1a12182ab3611f6dc66485b
Quinnie Liu <s224208977@deakin.edu.au>
2025-03-26 16:21:42 +1100

    add card

---
+++ Week3_Prac1/public/js/scripts.js
@@ -0,0 +1,40 @@
+const cardList = [
+    {
+        title: "pig",
+        image: "images/pig-dog-2613125_1280.jpg",
+        link: "About this pig",
+        description: "Demo description about pig"
+    },
+    {
+        title: "Puppy",
+        image: "images/puppy-1047521_1280.jpg",
+        link: "About this puppy",
+        description: "Demo description about puppy"
+    }
+]
+
+const clickMe = () => {
+    alert("Thanks for clicking me. Hope you have a nice day!")
+}
+
+const addCards = (items) => {
+    items.forEach(item =>{
+        let itemToAppend = '<div class="col s4 center-align">'+
+        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+        '</div><div class="card-content">'+
+        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
+        '<div class="card-reveal">'+
+            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+            '<p class="card-text">'+item.description+'</p>'+
+            '</div></div></div>';
+            $("#card-section").append(itemToAppend)
+    });
+}
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#clickMeButton').click( ()=>{
+        clickMe();
+    })
+    addCards(cardList);
+})
